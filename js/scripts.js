7d6029c069b47154edf8dcdda450b495c021e376
Mingwei Samuel <mingwei.samuel@gmail.com>
2015-08-26 18:51:30 +0000

    boilerplate

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var ref = new Firebase("https://phsasbvoter.firebaseio.com");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
