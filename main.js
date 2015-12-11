d579fbaeb6acc16ea1ea2b4bebf11a40a772cdea
Mingwei Samuel <mingwei.samuel@gmail.com>
2015-12-10 21:44:55 -0800

    chrome packaged app

---
+++ main.js
@@ -0,0 +1,98 @@
+window.onload = function() {
+  document.querySelector('#greeting').innerText =
+    'Hello, World! It is ' + new Date();
+
+  chrome.identity.getAuthToken({
+    interactive: true
+  }, function(token) {
+    if (chrome.runtime.lastError) {
+      alert(chrome.runtime.lastError.message);
+      return;
+    }
+    var x = new XMLHttpRequest();
+    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
+    x.onload = function() {
+      alert(x.response);
+      testWrite(x.response);
+    };
+    x.send();
+  });
+  
+  testWrite("test");
+};
+
+function testWrite(str) {
+  document.querySelector('#greeting').innerText += str;
+}
+
+//angular.module('VoterApp', ['ngMaterial']);
+
+angular
+  .module('VoterApp', ['ngMaterial'])
+  .controller('VoterController', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleLeft = buildDelayedToggler('left');
+    //$scope.toggleRight = buildToggler('right');
+    //$scope.isOpenRight = function(){
+    //  return $mdSidenav('right').isOpen();
+    //};
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+  });
+  /*
+  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  });*/
