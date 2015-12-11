a496f3b357c02c028efe6550abf508262a2fdfba
Mingwei Samuel <mingwei.samuel@gmail.com>
2015-12-10 21:22:50 -0800

    chrome packaged app

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-var ref = new Firebase("https://phsasbvoter.firebaseio.com");
-ref.authWithOAuthPopup("google", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
